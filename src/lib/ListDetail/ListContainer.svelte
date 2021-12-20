e3517595dc549ff18c8fa0d9f55a0898f04c0829
Peter Parsons <peterw.parsons@gmail.com>
2021-12-20 17:27:48 +0200

    wip on multiple column layout

--- src/lib/ListDetail/ListContainer.svelte
+++ src/lib/ListDetail/ListContainer.svelte
@@ -2,9 +2,9 @@
   import { fly } from 'svelte/transition'
 </script>
 
-<aside transition:fly={{ x: -200, duration: 200 }} class="relative xl:order-first flex flex-col flex-shrink-0 w-full md:w-96 border-r border-slate-700 overflow-y-auto">
+<aside transition:fly={{ x: -200, duration: 200 }} class="relative xl:order-first flex flex-col flex-shrink-0 w-full md:w-96 bg-white dark:bg-black-pearl border-r border-slate-200 dark:border-mirage overflow-y-auto">
   <div class="absolute inset-0 py-6 px-4 sm:px-6 lg:px-8">
-    <div class="h-full border-2 border-slate-500 border-dashed rounded-lg">
+    <div class="h-full border-2 border-slate-300 dark:border-slate-600 border-dashed rounded-lg">
     </div>
   </div>
 </aside>
