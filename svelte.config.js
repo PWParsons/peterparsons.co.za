e9a0c0e1d2ab9a521a2f3cc6e952e660b8dae4bb
Peter <peterw.parsons@gmail.com>
2022-03-06 12:09:54 +0200

    wip

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import preprocess from 'svelte-preprocess'
-import adapter from "@sveltejs/adapter-static";
+import adapter from "@sveltejs/adapter-static"
 
-const dev = process.env.NODE_ENV === 'development';
+const dev = true
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -12,8 +12,11 @@ const config = {
     }),
 
     paths: {
-      // change below to your repo name
       base: dev ? "" : "/peterparsons.me",
+    },
+
+    prerender: {
+      default: true,
     },
   },
 
